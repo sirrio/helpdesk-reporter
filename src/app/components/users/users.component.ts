f567092dd7d683c4eb0a0e8ef7e1c9556d74b77f
David Martin <home@sirrio.de>
2021-11-01 17:57:27 +0100

    added user controll and password change

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,11 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {AttendanceService} from '../../services/attendance.service';
 import {UserService} from '../../services/user.service';
 import {TokenStorageService} from '../../services/token-storage.service';
-import {User} from 'src/app/models/user.model';
-import {Role} from '../../models/role.model';
 import {map, mergeMap} from 'rxjs/operators';
-import {forkJoin} from 'rxjs';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -14,12 +11,13 @@ import {forkJoin} from 'rxjs';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  users: { isMod: any; isAdmin: any; isUser: any; username: any; }[] | undefined;
+  users: { id: any, isMod: any; isAdmin: any; isUser: any; username: any; pw: string }[] | undefined;
   isAdmin = false;
   isMod = false;
   isLoggedIn = false;
   username?: string;
   userid?: any;
+  passwordStr = '';
 
   constructor(private userService: UserService,
               private tokenStorageService: TokenStorageService) {
@@ -33,21 +31,53 @@ export class UsersComponent implements OnInit {
       this.userid = user.id;
       this.isAdmin = user.roles.includes('ROLE_ADMIN');
 
-      const tmp: { isMod: any; isAdmin: any; isUser: any; username: any; }[] | null | undefined = [];
-      const all = this.userService.getAll().pipe(
-        mergeMap(res => res),
-        map(u => {
-          const isAdmin = u.roles?.some(e => e.name === 'admin');
-          const isMod = u.roles?.some(e => e.name === 'moderator');
-          const isUser = u.roles?.some(e => e.name === 'user');
-          return {username: u.username, isAdmin, isMod, isUser};
-        }));
+      const tmp: { id: any, isMod: any; isAdmin: any; isUser: any; username: any; pw: string }[] | null | undefined = [];
+      const all = this.reload();
       all.subscribe(data => {
           tmp.push(data);
           this.users = tmp;
-          console.log(data);
         }
       );
+    }
+  }
+
+  reload(): Observable<{ isMod: any; pw: string; id: any; isAdmin: any; isUser: any; username: any }> {
+    const tmp: { id: any, isMod: any; isAdmin: any; isUser: any; username: any; pw: string }[] | null | undefined = [];
+    return this.userService.getAll().pipe(
+      mergeMap(res => res),
+      map(u => {
+        const isAdmin = u.roles?.some(e => e.name === 'admin');
+        const isMod = u.roles?.some(e => e.name === 'moderator');
+        const isUser = u.roles?.some(e => e.name === 'user');
+        return {id: u.id, username: u.username, isAdmin, isMod, isUser, pw: ''};
+      }));
+  }
+
+  toggleRole(id: any, role: string, rm: boolean): void {
+    this.userService.updateRole(id, role, rm).subscribe(() => {
+      const tmp: { id: any, isMod: any; isAdmin: any; isUser: any; username: any; pw: string }[] | null | undefined = [];
+      const all = this.reload();
+      all.subscribe(data => {
+          tmp.push(data);
+          this.users = tmp;
+        }
+      );
+    });
+  }
+
+  updatePassword(id: any, password: string): void {
+    if (password.length < 6) {
+      window.alert('Zu kurz');
+    } else {
+      this.userService.updatePassword(id, password).subscribe(() => {
+        const tmp: { id: any, isMod: any; isAdmin: any; isUser: any; username: any; pw: string }[] | null | undefined = [];
+        const all = this.reload();
+        all.subscribe(data => {
+            tmp.push(data);
+            this.users = tmp;
+          }
+        );
+      });
     }
   }
 }
